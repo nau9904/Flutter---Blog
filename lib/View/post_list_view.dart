4c7496c414584abea724e4ed69441a63f4f7e6c0
nau9904 <124415143+nau9904@users.noreply.github.com>
2025-06-19 14:44:54 +0900

    Create post_list_view.dart

---
+++ lib/View/post_list_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class PostListView extends StateLessWidget {
+	@override
+	Widget build(BuildContext context) {
+		final viewModel = Provider.of<PostViewModel>(context);
+
+		return Scaffold();
+	}
+
+
+}
